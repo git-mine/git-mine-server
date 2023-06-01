63bd8f545064e92f90a5491a3741c4369615dd7f
usama <mahmoodify@gmail.com>
2023-06-01 19:21:05 +0500

    adding console app for testing

---
+++ src/Frontend/GitMine.TestConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitMine.TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GitMine - TestConsole");
+        }
+    }
+}
