a1a40633e9a95b29d28ed6c5730271fe4ac8d781
usama <mahmoodify@gmail.com>
2023-06-07 13:42:22 +0500

    adding log level enum

---
+++ src/Core/GitMine.GitInterface/Interfaces/Types/LogLevel.cs
@@ -0,0 +1,23 @@
+﻿namespace GitMine.GitInterface.Interfaces.Types
+{
+    /// <summary>
+    /// Represents the level of any log message.
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// Normal or status level information.
+        /// </summary>
+        Information,
+
+        /// <summary>
+        /// Higher priority information.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// Critical information.
+        /// </summary>
+        Error
+    }
+}
