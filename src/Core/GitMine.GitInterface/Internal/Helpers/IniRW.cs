2a80aafa8b54268c441d5fafbd8bcdae016ee985
usama <mahmoodify@gmail.com>
2023-06-07 13:37:16 +0500

    adding class help info

--- src/Core/GitMine.GitInterface/Internal/Helpers/IniRW.cs
+++ src/Core/GitMine.GitInterface/Internal/Helpers/IniRW.cs
@@ -1,5 +1,9 @@
 ﻿namespace GitMine.GitInterface.Internal.Helpers
 {
+    /// <summary>
+    /// Class <c>IniRW</c> provides an easy to use interface
+    /// for reading and writing INI files.
+    /// </summary>
     internal class IniRW
     {
     }
