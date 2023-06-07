bc08d7c350d7d8573bc8cc0a4678e6a21e4ae89a
usama <mahmoodify@gmail.com>
2023-06-07 14:06:22 +0500

    adding string extensions

--- src/Core/GitMine.GitInterface/Internal/Extensions/StringExtensions.cs
+++ src/Core/GitMine.GitInterface/Internal/Extensions/StringExtensions.cs
@@ -1,6 +1,97 @@
-﻿namespace GitMine.GitInterface.Internal.Extensions
+﻿using System.Text.RegularExpressions;
+
+namespace GitMine.GitInterface.Internal.Extensions
 {
     internal static class StringExtensions
     {
+        private static Regex spacesPattern = new(@"\s+");
+
+        internal static bool IsEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value) ||
+                   string.IsNullOrWhiteSpace(value);
+        }
+
+        internal static string RemoveAllSpaces(this string value)
+        {
+            return spacesPattern.Replace(value, "");
+        }
+
+        internal static bool ContainsAnyIgnoringCase(this string text,
+            string text01,
+            string text02)
+        {
+            return text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text02, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        internal static bool ContainsAnyIgnoringCase(this string text,
+            string text01,
+            string text02,
+            string text03)
+        {
+            return text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text02, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text03, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        internal static bool ContainsAnyIgnoringCase(this string text,
+            string text01,
+            string text02,
+            string text03,
+            string text04)
+        {
+            return text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text02, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text03, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text04, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        internal static bool ContainsAnyIgnoringCase(this string text,
+            string text01,
+            string text02,
+            string text03,
+            string text04,
+            string text05)
+        {
+            return text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text02, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text03, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text04, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text05, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        internal static bool ContainsAnyIgnoringCase(this string text,
+            string text01,
+            string text02,
+            string text03,
+            string text04,
+            string text05,
+            params string[] textContd)
+        {
+            bool contains =
+                   text.Contains(text01, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text02, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text03, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text04, StringComparison.CurrentCultureIgnoreCase) ||
+                   text.Contains(text05, StringComparison.CurrentCultureIgnoreCase);
+
+            if (contains)
+            {
+                return true;
+            }
+            else
+            {
+                foreach (var item in textContd)
+                {
+                    if (text.Contains(item, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
     }
 }
