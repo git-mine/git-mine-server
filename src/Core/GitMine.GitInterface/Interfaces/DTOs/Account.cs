26fe08e050b9ffdfd11b5c81cc323fe62ddd6523
usama <mahmoodify@gmail.com>
2023-06-07 13:30:25 +0500

    adding id name and type

--- src/Core/GitMine.GitInterface/Interfaces/DTOs/Account.cs
+++ src/Core/GitMine.GitInterface/Interfaces/DTOs/Account.cs
@@ -1,6 +1,21 @@
-﻿namespace GitMine.GitInterface.Interfaces.DTOs
+﻿using GitMine.GitInterface.Interfaces.Types;
+
+namespace GitMine.GitInterface.Interfaces.DTOs
 {
     public class Account
     {
+        public string AccountId { get; }
+        public string AccountName { get; }
+        public AccountType AccountType { get; }
+
+        public Account(
+            string id,
+            string name,
+            AccountType accountType)
+        {
+            AccountId = id;
+            AccountName = name;
+            AccountType = accountType;
+        }
     }
 }
