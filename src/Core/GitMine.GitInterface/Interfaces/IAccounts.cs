9cca8e6f21ce0d3d415757b4afc462c0282d710d
usama <mahmoodify@gmail.com>
2023-06-09 14:38:54 +0500

    adding account related functions

--- src/Core/GitMine.GitInterface/Interfaces/IAccounts.cs
+++ src/Core/GitMine.GitInterface/Interfaces/IAccounts.cs
@@ -1,6 +1,12 @@
-﻿namespace GitMine.GitInterface.Interfaces
+﻿using GitMine.GitInterface.Interfaces.DTOs;
+
+namespace GitMine.GitInterface.Interfaces
 {
     public interface IAccounts
     {
+        Account GetAccountById(string accountId);
+        Account CreateAccount(Account account);
+        void UpdateAccount(Account account);
+        void DeleteAccount(Account account);
     }
 }
