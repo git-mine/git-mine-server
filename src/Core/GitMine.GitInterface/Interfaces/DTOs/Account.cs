d4be0952f7e3e19f37d2f28bf93e004220bc0e61
usama <mahmoodify@gmail.com>
2023-06-09 14:08:00 +0500

    adding description

--- src/Core/GitMine.GitInterface/Interfaces/DTOs/Account.cs
+++ src/Core/GitMine.GitInterface/Interfaces/DTOs/Account.cs
@@ -6,15 +6,18 @@ namespace GitMine.GitInterface.Interfaces.DTOs
     {
         public string Id { get; }
         public string Name { get; }
+        public string Description { get; }
         public AccountType Type { get; }
 
         public Account(
             string id,
             string name,
+            string description,
             AccountType accountType)
         {
             Id = id;
             Name = name;
+            Description = description;
             Type = accountType;
         }
     }
