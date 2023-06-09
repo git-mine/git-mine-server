c5e73bd2112383b0bfe8a62d1c3113e4c7d3015e
usama <mahmoodify@gmail.com>
2023-06-09 14:35:14 +0500

    simplifying the interface to support any number of messages in single output

--- src/Core/GitMine.GitInterface/Interfaces/ILogger.cs
+++ src/Core/GitMine.GitInterface/Interfaces/ILogger.cs
@@ -8,152 +8,28 @@ namespace GitMine.GitInterface.Interfaces
     public interface ILogger
     {
         /// <summary>
-        /// Writes <c cref="LogLevel.Information">Information</c>-level message to output log.
+        /// Writes <c cref="LogLevel.Information">Information</c>-level messages to output log.
         /// </summary>
-        /// <param name="message">The message.</param>
-        void Info(string message);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Information">Information</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        void Info(string message1, string message2);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Information">Information</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        void Info(string message1, string message2, string message3);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Information">Information</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        /// <param name="message4">Message #4 in the group.</param>
-        void Info(string message1, string message2, string message3, string message4);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Information">Information</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        /// <param name="message4">Message #4 in the group.</param>
-        /// <param name="message5">Message #5 in the group.</param>
-        void Info(string message1, string message2, string message3, string message4, string message5);
+        /// <param name="message">Log messages.</param>
+        void Info(params string[] message);
 
         /// <summary>
-        /// Writes <c cref="LogLevel.Warning">Warning</c>-level message to output log.
+        /// Writes <c cref="LogLevel.Warning">Warning</c>-level messages to output log.
         /// </summary>
-        /// <param name="message">The message.</param>
-        void Warning(string message);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Warning">Warning</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        void Warning(string message1, string message2);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Warning">Warning</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        void Warning(string message1, string message2, string message3);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Warning">Warning</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        /// <param name="message4">Message #4 in the group.</param>
-        void Warning(string message1, string message2, string message3, string message4);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Warning">Warning</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        /// <param name="message4">Message #4 in the group.</param>
-        /// <param name="message5">Message #5 in the group.</param>
-        void Warning(string message1, string message2, string message3, string message4, string message5);
+        /// <param name="message">Log messages.</param>
+        void Warning(params string[] message);
 
         /// <summary>
-        /// Writes <c cref="LogLevel.Error">Error</c>-level message to output log.
+        /// Writes <c cref="LogLevel.Error">Error</c>-level messages to output log.
         /// </summary>
-        /// <param name="message">The message.</param>
-        void Error(string message);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Error">Error</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        void Error(string message1, string message2);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Error">Error</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        void Error(string message1, string message2, string message3);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Error">Error</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        /// <param name="message4">Message #4 in the group.</param>
-        void Error(string message1, string message2, string message3, string message4);
-        /// <summary>
-        /// Writes <c cref="LogLevel.Error">Error</c>-level message group to output log.
-        /// </summary>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        /// <param name="message4">Message #4 in the group.</param>
-        /// <param name="message5">Message #5 in the group.</param>
-        void Error(string message1, string message2, string message3, string message4, string message5);
+        /// <param name="message">Log messages.</param>
+        void Error(params string[] messages);
 
         /// <summary>
-        /// Writes specified <c cref="LogLevel">LogLevel</c> message to output log.
+        /// Writes specified <c cref="LogLevel">LogLevel</c> messages to output log.
         /// </summary>
-        /// <param name="logLevel">Level of message.</param>
-        /// <param name="message">The message.</param>
-        void Log(LogLevel logLevel, string message);
-        /// <summary>
-        /// Writes specified <c cref="LogLevel">LogLevel</c> message group to output log.
-        /// </summary>
-        /// <param name="logLevel">Level of message group.</param>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        void Log(LogLevel logLevel, string message1, string message2);
-        /// <summary>
-        /// Writes specified <c cref="LogLevel">LogLevel</c> message group to output log.
-        /// </summary>
-        /// <param name="logLevel">Level of message group.</param>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        void Log(LogLevel logLevel, string message1, string message2, string message3);
-        /// <summary>
-        /// Writes specified <c cref="LogLevel">LogLevel</c> message group to output log.
-        /// </summary>
-        /// <param name="logLevel">Level of message group.</param>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        /// <param name="message4">Message #4 in the group.</param>
-        void Log(LogLevel logLevel, string message1, string message2, string message3, string message4);
-        /// <summary>
-        /// Writes specified <c cref="LogLevel">LogLevel</c> message group to output log.
-        /// </summary>
-        /// <param name="logLevel">Level of message group.</param>
-        /// <param name="message1">Message #1 in the group.</param>
-        /// <param name="message2">Message #2 in the group.</param>
-        /// <param name="message3">Message #3 in the group.</param>
-        /// <param name="message4">Message #4 in the group.</param>
-        /// <param name="message5">Message #5 in the group.</param>
-        void Log(LogLevel logLevel, string message1, string message2, string message3, string message4, string message5);
+        /// <param name="logLevel">Level of messages.</param>
+        /// <param name="messages">Log messages.</param>
+        void Log(LogLevel logLevel, params string[] messages);
     }
 }
