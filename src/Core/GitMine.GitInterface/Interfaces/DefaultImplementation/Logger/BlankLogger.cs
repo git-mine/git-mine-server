4508a3eb3a606972859e9af658ed1d0b941f615e
usama <mahmoodify@gmail.com>
2023-06-09 14:58:55 +0500

    adding doc info

--- src/Core/GitMine.GitInterface/Interfaces/DefaultImplementation/Logger/BlankLogger.cs
+++ src/Core/GitMine.GitInterface/Interfaces/DefaultImplementation/Logger/BlankLogger.cs
@@ -2,6 +2,10 @@
 
 namespace GitMine.GitInterface.Interfaces.DefaultImplementation.Logger
 {
+    /// <summary>
+    /// Class <c>BlankLogger</c> implements <c>ILogger</c> interface, though does nothing
+    /// with log messages. Use it to discard all the messages.
+    /// </summary>
     public class BlankLogger : ILogger
     {
         public void Info(params string[] messages)
