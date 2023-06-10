1c79fc1718bf9b09850334277d6a39fec199bd2e
usama <mahmoodify@gmail.com>
2023-06-10 15:38:38 +0500

    adding help info

--- src/Core/GitMine.GitInterface/Interfaces/IAccounts.cs
+++ src/Core/GitMine.GitInterface/Interfaces/IAccounts.cs
@@ -2,8 +2,16 @@
 
 namespace GitMine.GitInterface.Interfaces
 {
+    /// <summary>
+    /// Interface for handling user accounts.
+    /// </summary>
     public interface IAccounts
     {
+        /// <summary>
+        /// Checks if an account exists against the given ID or not.
+        /// </summary>
+        /// <param name="accountId">An account's ID.</param>
+        /// <returns>TRUE => Account by given ID exists. FALSE => Account doesn't exist.</returns>
         bool AccountIdExists(string accountId);
 
         Account GetAccountById(string accountId);
