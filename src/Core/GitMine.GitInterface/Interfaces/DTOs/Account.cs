a36a309e8cda9fda2b950966157d239f5691d0da
usama <mahmoodify@gmail.com>
2023-06-11 13:43:34 +0500

    adding help info for props

--- src/Core/GitMine.GitInterface/Interfaces/DTOs/Account.cs
+++ src/Core/GitMine.GitInterface/Interfaces/DTOs/Account.cs
@@ -4,10 +4,26 @@ namespace GitMine.GitInterface.Interfaces.DTOs
 {
     public class Account
     {
+        /// <summary>
+        /// A unique ID is assigned by the system upon account creation.
+        /// Once the account is created, the ID cannot be changed.
+        /// </summary>
         public string Id { get; }
+        /// <summary>
+        /// Name that is unique amongst different users.
+        /// </summary>
         public string UniqueName { get; }
+        /// <summary>
+        /// Any name that the user wants to use on profile.
+        /// </summary>
         public string DisplayName { get; }
+        /// <summary>
+        /// Details provided by the user to display on profile.
+        /// </summary>
         public string Description { get; }
+        /// <summary>
+        /// Type of the account.
+        /// </summary>
         public AccountType Type { get; }
 
         public Account(
