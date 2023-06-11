bee18d4a196970d239a8b87dff9ae424bd000d1f
usama <mahmoodify@gmail.com>
2023-06-11 13:29:25 +0500

    adding unique name for the account

--- src/Core/GitMine.GitInterface/Interfaces/DTOs/Account.cs
+++ src/Core/GitMine.GitInterface/Interfaces/DTOs/Account.cs
@@ -5,17 +5,20 @@ namespace GitMine.GitInterface.Interfaces.DTOs
     public class Account
     {
         public string Id { get; }
+        public string UniqueName { get; }
         public string DisplayName { get; }
         public string Description { get; }
         public AccountType Type { get; }
 
         public Account(
             string id,
+            string uniqueName,
             string displayName,
             string description,
             AccountType accountType)
         {
             Id = id;
+            UniqueName = uniqueName;
             DisplayName = displayName;
             Description = description;
             Type = accountType;
