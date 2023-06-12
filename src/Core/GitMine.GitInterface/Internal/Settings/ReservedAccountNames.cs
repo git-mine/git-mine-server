c39e5e4f0d1eb246c8cf262e9589d64c29c7e70f
usama <mahmoodify@gmail.com>
2023-06-12 14:44:31 +0500

    splitting account reserved names into separate fle within global settings class

---
+++ src/Core/GitMine.GitInterface/Internal/Settings/ReservedAccountNames.cs
@@ -0,0 +1,41 @@
+﻿namespace GitMine.GitInterface.Internal.Settings
+{
+    internal partial class GlobalSettings
+    {
+        public static readonly string[] ReservedAccountNames =
+        {
+            //- Git names
+            "git",
+            "gitignore", "gitattribute", "gitattributes",
+
+            //- Companies
+            "cronblocks", "innolixir", "ubrant", "gooan",
+            "github",
+            "oracle", "microsoft", "google",
+            "twitter", "facebook", "meta", "youtube",
+
+            //- OSes
+            "azurertos", "azure-rtos", "az-rtos", "azrtos",
+            "linux", "debian", "ubuntu", "redhat", "oracle-linux",
+            "raspbian",
+            "windows", "win",
+            "win7", "win8", "win8.1", "win10", "win11", "win12",
+            "win-7", "win-8", "win-8.1", "win-10", "win-11", "win-12",
+            "windows7", "windows8", "windows8.1", "windows10", "windows11", "windows12",
+            "windows-7", "windows-8", "windows-8.1", "windows-10", "windows-11", "windows-12",
+            
+            //- Technologies
+            "java", "javavm", "jvm",
+            "dotnet", "dotnetcore",
+
+            //- Programming and markup languages
+            "c", "c++", "cplusplus", "c_plusplus", "c_plus_plus",
+            "csharp", "c#", "css",
+            "fsharp", "f#",
+            "html",
+            "java", "javascript",
+            "shtml",
+            "python"
+        };
+    }
+}
