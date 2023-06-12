61c5ce60b4afea45304c681cd05b36433ea0c4bc
usama <mahmoodify@gmail.com>
2023-06-12 14:34:57 +0500

    adding reserved account names for blocking normal creation of given accounts

--- src/Core/GitMine.GitInterface/Internal/Settings/GlobalSettings.cs
+++ src/Core/GitMine.GitInterface/Internal/Settings/GlobalSettings.cs
@@ -2,5 +2,35 @@
 {
     internal class GlobalSettings
     {
+        public static readonly string[] ReservedAccountNames =
+        {
+            // Git names
+            "git", "gitignore", "gitattribute", "gitattributes",
+
+            // Companies
+            "cronblocks", "innolixir", "ubrant", "gooan",
+            "github",
+            "oracle", "microsoft", "google",
+            "twitter", "facebook", "meta", "youtube",
+
+            // Technologies
+            "java", "javavm", "jvm",
+            "dotnet", "dotnetcore",
+
+            // OSes
+            "azurertos", "azure-rtos", "az-rtos", "azrtos",
+            "linux", "debian", "ubuntu", "redhat", "oracle-linux",
+            "raspbian",
+            "windows",
+            "win7", "win8", "win8.1", "win10", "win11", "win12",
+            "win-7", "win-8", "win-8.1", "win-10", "win-11", "win-12",
+            "windows7", "windows8", "windows8.1", "windows10", "windows11", "windows12",
+            "windows-7", "windows-8", "windows-8.1", "windows-10", "windows-11", "windows-12",
+            "win",
+
+            // Programming languages
+            "c", "c++", "cplusplus", "c_plusplus", "c_plus_plus",
+            "python"
+        };
     }
 }
